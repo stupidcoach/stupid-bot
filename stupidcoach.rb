baaad516499d94a7cb89191653897b013868d413
tdeschamps <thomasdscmps@gmail.com>
2016-06-01 23:16:00 +0200

    added more parsing options and actions related to them

--- stupidcoach.rb
+++ stupidcoach.rb
@@ -3,12 +3,37 @@ require 'slack-ruby-bot'
 
 class PongBot < SlackRubyBot::Bot
   match /(ping|pong)/ do |client, data, match|
-    pingpong = ['ping', 'pong'].sample
+    pingpong = %w(ping pong).sample
     client.say(gif: pingpong, text: pingpong, channel: data.channel)
   end
 
   match /(ryan|robert|kirsten)/ do |client, data, match|
     client.say(gif: "#{match} #{rand(1..2) == 1 ? 'gosling' : 'boobs'}", channel: data.channel)
+  end
+
+  match /ruby|code/ do |client, data, match|
+    language = %w(ruby javascript rust elixir python).sample
+    client.say(gif: language, text: ":troll: #{language} :troll:", channel: data.channel)
+  end
+
+  match /tenderness/ do |client, data, match|
+    client.say(gif: 'camille rowe', text: 'Camille in a row', channel: data.channel)
+  end
+
+  match /lovelyness/ do |client, data, match|
+    client.say(gif: 'kirsten stewart', text: 'Kirsten :heart: vampire', channel: data.channel)
+  end
+
+  match /awesomeness/ do |client, data, match|
+    client.say(gif: 'ryan hey girl', text: 'He drives', channel: data.channel)
+  end
+
+  match /soso/ do |client, data, match|
+    client.say(gif: 'matthew mcconaughey', text: '6pack and a soul', channel: data.channel)
+  end
+
+  match /dog/ do |client, data, match|
+    client.say(gif: 'cute akita', text: 'All you need is dog', channel: data.channel)
   end
 end
 
